c47a7c45149d0fc25792ce2e9307dcd6400a9fc3
zdaiot <zdaiot@163.com>
2023-05-10 18:51:40 +0800

    add CustomASTVisitor

--- code_ast/tests/test_tq.py
+++ code_ast/tests/test_tq.py
@@ -2,12 +2,75 @@ import sys
 sys.path.insert(0, './code_ast')
 import code_ast
 print(code_ast)
-from code_ast import ast
+from code_ast import ast, match_span
+from code_ast import ASTVisitor
+
+
+def get_length_cum(code_str):
+    codes = code_str.split('\n')
+    lines_length = [len(x)+1 for idx, x in enumerate(codes) if idx<len(codes)-1]
+    cum = 0
+    lines_length_cum = []
+    for line_length in lines_length:
+        cum += line_length
+        lines_length_cum.append(cum)
+    return lines_length_cum
+
+
+def convert_xy_x(xy: tuple, lines_length_cum: list):
+    x, y = xy
+    if x == 0:
+        new_x = y
+    else:
+        new_x = lines_length_cum[x-1] + y
+    return new_x
+
+
+def print_part_code(xy_xy_str, code_str):
+    lines_length_cum = get_length_cum(code_str)
+    xy_xy = xy_xy_str.split('-')
+    xy1 = eval(xy_xy[0])
+    x1 = convert_xy_x(xy1, lines_length_cum)
+    xy2 = eval(xy_xy[1])
+    x2 = convert_xy_x(xy2, lines_length_cum)
+    print(code_str[x1: x2+1])
+
+
+class CustomASTVisitor(ASTVisitor):
+    def __init__(self, code_str):
+        self._code_str = code_str
+        self._code_str_split = code_str.splitlines()
+        self._lines_length_cum = get_length_cum(self._code_str)
+
+    def get_part_code(self, start_point, end_point, print_flag=True):
+        xs = convert_xy_x(start_point, self._lines_length_cum)
+        xe = convert_xy_x(end_point, self._lines_length_cum)
+        part_code = self._code_str[xs: xe+1]
+        if print_flag:
+            print(str(start_point)+', '+str(end_point))
+            print(part_code)
+        return part_code
+
+    def get_node_info(self, node, print_flag=True):
+        xs = convert_xy_x(node.start_point, self._lines_length_cum)
+        xe = convert_xy_x(node.end_point, self._lines_length_cum)
+        part_code = self._code_str[xs: xe]
+        # part_code = match_span(node, self._code_str_split)
+        if print_flag:
+            print(node)
+            print(part_code)
+        return part_code
+
+    def visit(self, node):
+        self.get_node_info(node)
+        print()
 
 
 def run_code_cst(code_str: str, lang: str="cpp"):
     # Parse the code
     source_ast = ast(code_str, lang=lang, rebuild=True, syntax_error='warn')
+    count_visitor = CustomASTVisitor(code_str)
+    source_ast.visit(count_visitor)
     return source_ast
 
 
@@ -33,6 +96,13 @@ if __name__ == '__main__':
     }
     return typedArray;
     }
+    '''
+
+    code = '''
+@export
+macro PrintHelloWorld(): void {
+Print('Hello world!');
+}
     '''
 
     visit_result = run_code_cst(code, lang='torque')
